5a4ab1138db6c31381c6761003ef55be3510d493
David Silva <david.dacostaesilva@gmail.com>
2025-09-27 13:05:33 -0300

    66. Exercício guiado - Calculadora - Parte 1

---
+++ Seção 3 Iniciando na programação com Python (Lógica de programação básica)/aula040.py
@@ -0,0 +1,16 @@
+""" 
+66. Exercício guiado - Calculadora - Parte 1
+
+
+Calculadora com while 
+
+
+
+"""
+while True:
+    print('nummmmm')
+    #########
+    sair = input('Quer sair? [s]im: ').lower().startswith('s')
+
+    if sair is True:
+        break
