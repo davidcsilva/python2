ec37b9071241d5abc3a0fda9b7a4574e3b181abc
David Silva <david.dacostaesilva@gmail.com>
2025-09-27 08:10:22 -0300

    50. Parte 1: Variáveis, constantes e complexidade de código

---
+++ Seção 3 Iniciando na programação com Python (Lógica de programação básica)/aula030.py
@@ -0,0 +1,13 @@
+"""
+50. Parte 1: Variáveis, constantes e complexidade de código
+
+CONSTANTE = "Variáveis" que não vão mudar
+Muitas condições no mesmo if (ruim)
+    <- Contagem de complexidade (ruim)
+"""
+velocidade = 61  # velocidade atual do carro
+local_carro = 100  # local em que o carro está na estrada
+
+RADAR_1 = 60  # velocidade máxima do radar 1
+LOCAL_1 = 100  # local onde o radar 1 está
+RADAR_RANGE = 1  # A distância onde o radar pega
