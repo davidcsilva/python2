27349163fe441e12e695714195c7ab8333fcd47b
David Silva <david.dacostaesilva@gmail.com>
2025-09-27 12:48:50 -0300

    59. while e break - Estrutura de repetição (Parte 1)

---
+++ Seção 3 Iniciando na programação com Python (Lógica de programação básica)/aula034.py
@@ -0,0 +1,18 @@
+"""
+59. while e break - Estrutura de repetição (Parte 1)
+
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+condicao = True
+
+while condicao:
+    nome = input('Qual o seu nome: ')
+    print(f'Seu nome é {nome}')
+
+    if nome == 'sair':
+        break
+
+print('Acabou')
