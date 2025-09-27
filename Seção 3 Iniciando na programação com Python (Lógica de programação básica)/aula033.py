3d48afd6e4860b47f099d565d234e51957257ebd
David Silva <david.dacostaesilva@gmail.com>
2025-09-27 12:47:37 -0300

    58. Conversa - tipos built-in, documentação, tipo imutáveis, métodos de string

---
+++ Seção 3 Iniciando na programação com Python (Lógica de programação básica)/aula033.py
@@ -0,0 +1,11 @@
+"""
+58. Conversa - tipos built-in, documentação, tipo imutáveis, métodos de string
+
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+string = '1000'
+# outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# print(string)
+# print(outra_variavel)
+print(string.zfill(10))
