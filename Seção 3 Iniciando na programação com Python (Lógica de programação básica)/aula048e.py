69398e2eed18c736630e200b35112b7137a04bc1
David Silva <david.dacostaesilva@gmail.com>
2025-10-04 10:39:03 -0300

    84. Cuidados com tipos de dados mutáveis - list e copy

---
+++ Seção 3 Iniciando na programação com Python (Lógica de programação básica)/aula048e.py
@@ -0,0 +1,13 @@
+"""
+84. Cuidados com tipos de dados mutáveis - list e copy
+
+Cuidados com dados mutáveis
+= - copiado o valor (imutáveis)
+= - aponta para o mesmo valor na memória (mutável)
+"""
+lista_a = ['Luiz', 'Maria', 1, True, 1.2]
+lista_b = lista_a.copy()
+
+lista_a[0] = 'Qualquer coisa'
+print(lista_a)
+print(lista_b)
