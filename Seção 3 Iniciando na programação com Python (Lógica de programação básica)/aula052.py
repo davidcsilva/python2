36b8fb1c05671d8138e220070e96efdf9b7d1e3f
David Silva <david.dacostaesilva@gmail.com>
2025-10-04 10:44:26 -0300

    88. Tipo tuple (tuplas)

---
+++ Seção 3 Iniciando na programação com Python (Lógica de programação básica)/aula052.py
@@ -0,0 +1,11 @@
+"""
+88. Tipo tuple (tuplas)
+
+
+Tipo tupla - Uma lista imutável
+"""
+nomes = ('Maria', 'Helena', 'Luiz')
+# nomes = tuple(nomes)
+# nomes = list(nomes)
+print(nomes[-1])
+print(nomes)
