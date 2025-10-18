b0325a555a88946f15e0b0aa94ee2b5a434a5c3a
David Silva <david.dacostaesilva@gmail.com>
2025-10-18 10:27:45 -0300

    150. (Parte 1) try e except para tratar exceções

---
+++ Seção 4 Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional e +/aula093.py
@@ -0,0 +1,28 @@
+'''
+150. (Parte 1) try e except para tratar exceções
+
+'''
+
+
+# (Parte1) try e except para tratar exceções
+# a = 18
+# b = 0
+# c = a / b
+
+try:
+    a = 18
+    b = 0
+    # print(b[0])
+    print('Linha 1'[1000])
+    c = a / b
+    print('Linha 2')
+except ZeroDivisionError:
+    print('Dividiu por zero.')
+except NameError:
+    print('Nome b não está definido')
+except (TypeError, IndexError):
+    print('TypeError + IndexError')
+except Exception:
+    print('ERRO DESCONHECIDO.')
+
+print('CONTINUAR')
