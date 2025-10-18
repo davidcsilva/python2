c329e07b4688937258fad9b9f46b90559b2966be
David Silva <david.dacostaesilva@gmail.com>
2025-10-18 10:00:56 -0300

    131. Exemplo de uso do tipo set

---
+++ Seção 4 Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional e +/aula079.py
@@ -0,0 +1,16 @@
+'''
+131. Exemplo de uso do tipo set
+
+'''
+
+# Exemplo de uso dos sets
+letras = set()
+while True:
+    letra = input('Digite: ')
+    letras.add(letra.lower())
+
+    if 'l' in letras:
+        print('PARABÉNS')
+        break
+
+    print(letras)
